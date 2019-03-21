fc4838b748fcd9dfb85c07277be0ad0e0d66812e
Baraa Orabi <baraaorabi@gmail.com>
2019-03-21 15:13:46 -0700

    Simplied code and resolved bugs in intervals

--- py/get_nanosim_tsv.py
+++ py/get_nanosim_tsv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import itertools
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -32,7 +33,21 @@ def get_transcript_infos(transcript_tsv):
         transcript_infos[tid]['strand'] = line[2]
         transcript_infos[tid]['exons'] = [(int(x.split('-')[0]),int(x.split('-')[1])) for x in line[3].rstrip(',').split(',')]
         transcript_infos[tid]['length'] = sum([x[1]-x[0] for x in transcript_infos[tid]['exons']])
+        transcript_infos[tid]['genic_pos'] = [0]*transcript_infos[tid]['length']
+        transcript_pos = 0
+        for (start,end) in transcript_infos[tid]['exons']:
+            for genic_pos in range(start,end):
+                transcript_infos[tid]['genic_pos'][transcript_pos] = genic_pos
+                transcript_pos+=1
     return transcript_infos
+
+# Modified from https://www.geeksforgeeks.org/python-make-a-list-of-intervals-with-sequential-numbers/
+def intervals_extract(iterable):
+    iterable = sorted(set(iterable))
+    func = lambda t: t[1] - t[0]
+    for key, group in itertools.groupby(enumerate(iterable),func):
+        group = list(group)
+        yield (group[0][1], group[-1][1]+1)
 
 def output_nanosim_reads_tsv(transcript_infos, nanosim_reads_fasta, out_path):
     if out_path == None:
@@ -47,58 +62,22 @@ def output_nanosim_reads_tsv(transcript_infos, nanosim_reads_fasta, out_path):
         comment = line[1:].rstrip().split()[1]
         line = rname.split('_')
         tid = line[0]
-        start_on_transcript = int(line[1])
+        start = int(line[1])
         aligned = line[2] == 'aligned'
         rid = int(line[3])
         strand = line[4]
         ssc = int(line[5])
         aln = int(line[6])
         esc = int(line[7])
-        gene_starts = list()
-        gene_ends = list()
-        r_measured = 0
-        t_measured = 0
-        eidx = 0
-        # Finding the first exon start
-        for exon in transcript_infos[tid]['exons']:
-            eidx += 1
-            exon_len = exon[1]-exon[0]
-            t_measured += exon_len
-            if start_on_transcript < t_measured:
-                offset = start_on_transcript - (t_measured - exon_len)
-                gene_starts.append(exon[0] + offset)
-                if aln - r_measured >= t_measured - start_on_transcript:
-                    gene_ends.append(exon[1])
-                    r_measured += t_measured - start_on_transcript
-                else:
-                    gene_ends.append(gene_starts[-1]+ (aln-r_measured))
-                    r_measured += gene_ends[-1]-gene_starts[-1]
-                break
-        # From first exon start till last exon end
-        for exon in transcript_infos[tid]['exons'][eidx:]:
-            eidx+=1
-            exon_len = exon[1]-exon[0]
-            t_measured += exon_len
-            if aln-r_measured <= 0:
-                break
-            gene_starts.append(exon[0])
-            if aln-r_measured > exon_len:
-                gene_ends.append(exon[1])
-                r_measured+=exon_len
-            else:
-                gene_ends.append(gene_starts[-1] + (aln-r_measured))
-                r_measured += gene_ends[-1]-gene_starts[-1]
-        assert(r_measured == aln)
-        for exon in transcript_infos[tid]['exons'][eidx:]:
-            exon_len = exon[1]-exon[0]
-            t_measured += exon_len
-        assert(t_measured == transcript_infos[tid]['length'])
-        exons = ','.join(('{}-{}'.format(x[0],x[1]) for x in zip(gene_starts, gene_ends)))
-        print('\t'.join((rname, tid, strand,exons)), file=out_file)
+        genic_positions = (transcript_infos[tid]['genic_pos'][start:start+aln])
+        exons = list(intervals_extract(genic_positions))
+        exons = ','.join(('{}-{}'.format(e[0],e[1]) for e in exons))
+        print('\t'.join((rname, tid, strand, exons)), file=out_file)
 
 def main():
     args = parse_args()
     transcript_infos = get_transcript_infos(args.transcript_tsv)
     output_nanosim_reads_tsv(transcript_infos, args.nanosim_reads, args.output)
+
 if __name__ == "__main__":
     main()
