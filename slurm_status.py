cd344e4fd7870ae49a3b66241e96c658e838aaea
Baraa Orabi <baraaorabi@gmail.com>
2019-08-20 10:45:56 -0700

    script to let Snakemake check Slurm status

---
+++ slurm_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+
+jobid = sys.argv[1]
+
+output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
+
+running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
+if "COMPLETED" in output:
+    print("success")
+elif any(r in output for r in running_status):
+    print("running")
+else:
+    print("failed")
+
