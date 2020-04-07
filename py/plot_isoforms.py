d53e8c01fb9af7b610899f075905d7d50c25fdcd
Baraa Orabi <baraaorabi@gmail.com>
2020-04-06 22:34:37 -0700

    getting ch and softclip info

--- py/plot_isoforms.py
+++ py/plot_isoforms.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.patches as patches
 from PyPDF2 import PdfFileMerger, PdfFileReader
+from dateutil import parser
 
 colors = [
     '#a6cee3',
@@ -21,6 +22,17 @@ colors = [
     '#6a3d9a',
     '#ffff99',
     '#b15928',
+]
+colors_ch = [
+    '#999999',
+    '#e41a1c',
+    '#377eb8',
+    '#4daf4a',
+    '#984ea3',
+    '#ff7f00',
+    '#ffff33',
+    '#a65628',
+    '#f781bf',
 ]
 grid_width_ratios = [
     (2000, 8),
@@ -46,8 +58,18 @@ def parse_args():
                         type=str,
                         required=True,
                         help="Path to TXT file of canonical exons")
+    parser.add_argument("-q",
+                        "--fastq",
+                        type=str,
+                        required=True,
+                        help="Path to FASTQ file of reads")
+    parser.add_argument("-p",
+                        "--paf",
+                        type=str,
+                        required=True,
+                        help="Path to PAF file of reads")
     parser.add_argument("-i",
-                        "--isoforms_tsv",
+                        "--isoforms-tsv",
                         type=str,
                         required=True,
                         help="Path to TSV file of read isoform assignments")
@@ -58,6 +80,28 @@ def parse_args():
                         help="Output prefix that does not include .EXT part")
     args = parser.parse_args()
     return args
+
+def get_softclip(paf):
+    rname_to_sc = dict()
+    for line in open(paf):
+        line = line.rstrip().split('\t')
+        name = line[0]
+        if not name in rname_to_sc:
+            rname_to_sc[name] = dict(
+                length  = int(line[1]),
+                strand  = line[4],
+                q_start = int(line[2]),
+                q_end   = int(line[3]),
+            )
+        rname_to_sc[name]['q_start'] = min(rname_to_sc[name]['q_start'], int(line[2]))
+        rname_to_sc[name]['q_end']   = max(rname_to_sc[name]['q_end'], int(line[3]))
+        if rname_to_sc[name]['strand'] == '+':
+            rname_to_sc[name]['scs']=rname_to_sc[name]['q_start']
+            rname_to_sc[name]['sce']=rname_to_sc[name]['length']-rname_to_sc[name]['q_end']-1
+        else:
+            rname_to_sc[name]['scs']=rname_to_sc[name]['length']-rname_to_sc[name]['q_end']-1
+            rname_to_sc[name]['sce']=rname_to_sc[name]['q_start']
+    return rname_to_sc
 
 def get_tinfo(segments_txt, transcripts_tsv):
     segs = [int(l.strip()) for l in open(segments_txt)]
@@ -76,6 +120,28 @@ def get_tinfo(segments_txt, transcripts_tsv):
     tid_to_color = {tid:colors[tidx%len(colors)] for tidx,tid in enumerate(sorted(tid_to_segs.keys()))}
     return segs,grid_lens,tid_to_segs,tid_to_color
 
+def read_fastq(fastq):
+    rname_to_info = dict()
+    for line in open(fastq):
+        if line[0]!='@':
+            continue
+        line = line[1:].rstrip().split()
+        rname = line[0]
+        info=dict(
+            runid=0,
+            read=0,
+            ch=0,
+            start_time="1970-01-01T00:00:00Z",
+        )
+        for x in line[1:]:
+            x = x.split('=')
+            if x[0] in info:
+                info[x[0]] = x[1]
+        info['ch'] = int(info['ch'])
+        info['start_time'] = parser.parse(info['start_time']).timestamp()
+        rname_to_info[rname]=info
+    return rname_to_info
+
 def get_isoforms(isoforms_tsv):
     reads = dict()
     isoforms = dict()
@@ -91,7 +157,12 @@ def get_isoforms(isoforms_tsv):
         rname = line[1]
         rid = int(line[2])
         d = [x=='1' for x in line[3]]
-        reads[rid]=dict(iid=iid, name=rname, data=d, tname=rname.split('_')[0])
+        tname = rname.split('_')[0]
+        if len(tname)==len('ENST00000396043') and tname[0:4]=='ENST' and tname[5:].isdigit():
+            pass
+        else:
+            tname=''
+        reads[rid]=dict(iid=iid, name=rname, data=d, tname=tname)
         isoforms[iid]['rids'].append(rid)
     isoforms = [isoforms[x] for x in sorted(isoforms.keys())]
     return isoforms,reads
@@ -118,12 +189,24 @@ def plot_isoforms(isoform, grid_lens, tid_to_segs, segs, reads, tid_to_color, ou
             else:
                 ax.set_yticks([])
 
-    rid_to_p = {rid:p for p,(_,rid) in enumerate(sorted([(reads[rid]['name'],rid) for rid in isoform['rids']]))}
+    rid_to_p = {rid:p for p,(tname,ch,data,rid) in enumerate(sorted(
+        [(
+            reads[rid]['tname'],
+            reads[rid]['data'],
+            reads[rid]['ch'],
+            rid
+        ) for rid in isoform['rids']]
+    ))}
     for aid,ax in enumerate(r_axes):
         for rid in isoform['rids']:
             if reads[rid]['data'][aid]==False:
                 continue
-            ax.add_patch(patches.Rectangle((0,rid_to_p[rid]),1,1,color=tid_to_color.get(reads[rid]['tname'],'gray')))
+            ax.add_patch(patches.Rectangle(
+                xy     = (0,rid_to_p[rid]),
+                width  = 1,
+                height = 1,
+                color  = tid_to_color.get(reads[rid]['tname'], colors_ch[reads[rid]['ch']%len(colors_ch)])
+            ))
 
     tid_to_p = {tid:p for p,tid in enumerate(sorted(tid_to_segs.keys()))}
     for tid,intervals in tid_to_segs.items():
@@ -150,6 +233,8 @@ def plot_isoforms(isoform, grid_lens, tid_to_segs, segs, reads, tid_to_color, ou
 
 def main():
     args = parse_args()
+    rname_to_info = read_fastq(args.fastq)
+    rname_to_sc   = get_softclip(args.paf)
     segs,grid_lens,tid_to_segs,tid_to_color=get_tinfo(
         segments_txt=args.segments_txt,
         transcripts_tsv=args.transcripts_tsv
@@ -157,7 +242,18 @@ def main():
     isoforms,reads=get_isoforms(
         isoforms_tsv=args.isoforms_tsv
     )
-
+    for rid,read in reads.items():
+        rname = read['name']
+        assert rname in rname_to_info
+        for k,v in rname_to_info[rname].items():
+            assert not k in read
+            reads[rid][k]=v
+        assert rname in rname_to_sc
+        reads[rid][k]['scs'] = rname_to_sc[rname]['scs']
+        reads[rid][k]['sce'] = rname_to_sc[rname]['sce']
+        reads[rid][k]['strand'] = rname_to_sc[rname]['strand']
+    # for read in reads.values():
+    #     print(read)
     for iid,isoform in enumerate(isoforms):
         print('Plotting isoform {}...'.format(iid))
         plot_isoforms(
